c6fe6fb324e5034c4472d227e7fb8404ec724146
heshiyu <heshiyu@corp.netease.com>
2020-02-06 15:48:14 +0800

    refactor: src/components

--- src/view/intro/index.js
+++ src/view/intro/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App';
-import NotFound from '@/components/common/not-found';
+import NotFound from '@/components/not-found';
 import 'core-js'; // 兼容IE处理
 import '@/assets/iconfont/iconfont.css';
 import '@/assets/css/index.less';
