5b964e2df0ecebc97836102d7e2f9b841ddf337e
heshiyu <450979309@qq.com>
2020-02-20 11:04:04 +0800

    refactor: Remove Page.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Page from './Page';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import NotFound from '@/view/not-found';
+import App from './App';
 import 'core-js'; // 兼容IE处理
 import '@/assets/iconfont/iconfont.css';
 import '@/assets/css/index.less';
+
+const Page = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" render={() => <Redirect to="/app/index" push />} />
+            <Route path="/app" component={App} />
+            <Route component={NotFound} />
+        </Switch>
+    </Router>
+);
 
 ReactDOM.render(<Page />, document.getElementById('root'));
