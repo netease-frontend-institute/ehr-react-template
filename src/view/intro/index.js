07c35974225547ae9e1828d511b5cb4b6d9d9291
heshiyu <450979309@qq.com>
2020-02-20 11:27:16 +0800

    refactor: Optimize components with ts 100%

--- src/view/intro/index.js
+++ src/view/intro/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './../_common/App';
-import NotFound from '@/components/not-found';
+import NotFound from '@/view/not-found';
 import './../_common/mixins';
 import '@/assets/iconfont/iconfont.css';
 import '@/assets/css/index.less';
